5289a909d7ebcd67605695dc41394cf0e6af924b
matt.ogden <mogd001@gmail.com>
2022-08-08 15:40:54 +1200

    Modified output to simplify upload to Hilltop

--- merge-bam-data/merge_dat_files.R
+++ merge-bam-data/merge_dat_files.R
@@ -5,28 +5,31 @@ library(plotly)
 merge_dat_files <- function(files) {
   # Merge a list of .dat files with equal structure.
   merge_df <- NULL
-  
+
   for (f in files) {
-    df <- read.table(f, head = FALSE, skip = 5) 
-    names(df) <- df[1,]
-    df <- df[-1,]
-    
+    df <- read.table(f, head = FALSE, skip = 5)
+    names(df) <- df[1, ]
+    df <- df[-1, ]
+
     merge_df <- rbind(merge_df, df)
   }
-  
+
   rownames(merge_df) <- 1:nrow(merge_df)
-  merge_df %>% 
-    distinct() %>% 
-    mutate(Date = mdy(Date),
-           Time = hm(Time), 
-           datetime = Date + Time,
-           Time = format(datetime, format = "%H:%M:%S")) %>% 
-    arrange(datetime)
+  merge_df %>%
+    distinct() %>%
+    mutate(
+      Date = mdy(Date),
+      Time = hm(Time),
+      datetime = Date + Time,
+      Time = format(datetime, format = "%H:%M:%S")
+    ) %>%
+    arrange(datetime) %>%
+    tibble()
 }
 
 # Short and Long files. Short, is if the filename contains "short".
 directory <- "M:/Datafiles/Downloads&OtherSources/AIRQUALITY/Richmond/Plunket/5028i BAM/(024) 5028i Download 02-08-2022"
-filenames <- list.files(directory, pattern="*.dat", full.names=TRUE)
+filenames <- list.files(directory, pattern = "*.dat", full.names = TRUE)
 
 short_filenames <- filenames[grepl("short", tolower(filenames))]
 long_filenames <- filenames[!grepl("short", tolower(filenames))]
@@ -34,28 +37,51 @@ long_filenames <- filenames[!grepl("short", tolower(filenames))]
 day <- format(Sys.time(), "%Y%m%d")
 
 # save to csv
-short <- merge_dat_files(short_filenames)
-short %>% 
+short <- merge_dat_files(short_filenames) %>%
+  select(datetime, humidity = ambrh)
+short %>%
   write.csv(paste0("outputs/", day, "_short_5028i.csv"))
 # only output selected columns
 
-long <- merge_dat_files(long_filenames) 
-long %>% 
-  write.csv(paste0("outputs/", day, "_long_5028i.csv"))
+long <- merge_dat_files(long_filenames) %>%
+  mutate(
+    pm = as.numeric(pm),
+    pmb = as.numeric(pmb),
+    avgpm = as.numeric(avgpm),
+    avgpmb = as.numeric(avgpmb)
+  )
 
+long_5min <- long %>%
+  select(datetime, pm10 = pm, pm2p5 = pmb)
+long_5min %>%
+  write.csv(paste0("outputs/", day, "_long_5min_5028i.csv"))
+
+long_daily <- long %>%
+  group_by(Date) %>%
+  mutate(pm10_daily_calc = mean(pm, na.rm = TRUE), pm2p5_daily_calc = mean(pmb, na.rm = TRUE)) %>%
+  ungroup() %>%
+  subset(Time != "00:00:00") %>%
+  select(Date, pm10_daily = avgpm, pm2p5_daily = avgpmb, pm10_daily_calc, pm2p5_daily_calc) %>%
+  distinct() %>%
+  mutate(
+    pm10_daily_calc = lag(pm10_daily_calc),
+    pm2p5_daily_calc = lag(pm2p5_daily_calc),
+    datetime = Date - days(1) + hours(23) + minutes(59) + seconds(59)
+  ) %>%
+  select(datetime, pm10_daily, pm2p5_daily, pm10_daily_calc, pm2p5_daily_calc)
+
+long_daily %>%
+  write.csv(paste0("outputs/", day, "_long_daily_5028i.csv"))
 
 # visualise for testing
-short <- tibble(short) %>%
-  mutate(pm = as.numeric(pm))
+short <- short %>%
+  mutate(humidity = as.numeric(humidity))
 
-p_short<- ggplot(long, aes(Date, pm)) + 
-  geom_point(size = 1, color = "red") +
+p_short <- ggplot(short, aes(datetime, humidity)) +
+  geom_point(size = 1, color = "blue") +
   scale_y_continuous(limits = c(0, NA))
 
-long <- tibble(long) %>% 
-  mutate(pm = as.numeric(pm))
-
-p_long <- ggplot(long, aes(Date, pm)) + 
+p_long <- ggplot(long_5min, aes(datetime, pm10)) +
   geom_point(size = 1, color = "red") +
   scale_y_continuous(limits = c(0, NA))
 
