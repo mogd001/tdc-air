773fdef1dae0320cd0adc219aede36fe44198eb9
matt.ogden <mogd001@gmail.com>
2022-09-30 16:03:21 +1300

    Annual reporting update

--- annual-reporting/02_modelling.R
+++ annual-reporting/02_modelling.R
@@ -1,6 +1,11 @@
 library(knitr)
+library(tidyverse)
+library(lubridate)
 
-source("01_load_aq_data.R")
+#source("01_load_aq_data.R")
+#saveRDS(aq_day, file = "temp.RDS") 
+
+aq_day <- readRDS("temp.RDS")
 
 site <- "AQ Richmond Central at Plunket"
 aq_day_richmond_pm10 <- filter(aq_day, site == !!site & measurement == "PM10") %>% 
@@ -9,13 +14,12 @@ aq_day_richmond_pm10 <- filter(aq_day, site == !!site & measurement == "PM10") %
 aq_day_richmond_pm10
 View(aq_day_richmond_pm10)
 
-
 # summary stats
-
 summary_stats <- group_by(aq_day_richmond_pm10, year) %>%
   summarise(
     count = n(),
-    count_non_na = sum(!is.na(value)),
+    count_non_na = sum(!is.na(value), na.rm = TRUE),
+    count_exceedance = sum(value > 50, na.rm = TRUE),
     mean = mean(value, na.rm = TRUE),
     sd = sd(value, na.rm = TRUE),
     median = median(value, na.rm = TRUE),
@@ -29,7 +33,78 @@ kruskal.test(value ~ year, data = aq_day_richmond_pm10)
 # Interpretation: as the p-value is less than the significance level 0.05, we can conclude that there are significant differences between the treatment groups.
 pwt <- pairwise.wilcox.test(aq_day_richmond_pm10$value, aq_day_richmond_pm10$year,
                      p.adjust.method = "BH")
-ifelse(pwt$p.value < 0.05, 1, 0)
-# Interpretation: the pairwise comparison shows that, any combination of years where p < 0.05  are significantly different.
+ifelse(pwt$p.value < 0.05, 1, 0) # Interpretation: the pairwise comparison shows that, any combination of years where p < 0.05  are significantly different.
 
+#### Supervised Classification
+all_data <- aq_day_richmond_pm10 %>% 
+  mutate(pm10 = value) %>% 
+  mutate_at(vars(wind_mps_nhrs_less1, wind_mps_nhrs_less2, wind_mps_nhrs_less3, temp_degc_nhrs_less1, temp_degc_nhrs_less5, temp_degc_nhrs_less10), ~replace_na(., 0)) %>% 
+  drop_na() %>% 
+  #select(pm10, wind_mps_24h_avg, temp_degc_4h_avg) %>%  # reducing variables initially for first model development
+  mutate(pm10 = if_else(pm10 <= 50, "pm10_below_50", "pm10_above_50")) %>% # covert to binary outcome for supervised classification
+  mutate(pm10 = factor(pm10, levels = c("pm10_below_50", "pm10_above_50")))
 
+ggplot(all_data, aes(wind_mps_24h_avg, temp_degc_4h_avg, value)) +
+  geom_hex(bins = 70) +
+  scale_fill_continuous(type = "viridis") +
+  theme_bw() +
+  facet_wrap(~pm10)
+
+all_data %>% 
+  filter(pm10 == "pm10_above_50") %>% 
+  group_by(year) %>% 
+  summarise(n = n()) %>% 
+  ungroup() %>% 
+  ggplot(aes(year, n)) +
+  geom_point() +
+  theme_bw() +
+  
+model_data <- all_data %>% 
+  select(-c(date, site, measurement, reporting_period, year, value)) %>% 
+  select(pm10, temp_degc_4h_avg, wind_mps_24h_avg)
+
+# Split into training and test dataset
+train_size <- round(0.75 * nrow(model_data), 0)
+split <- sample(c(rep(0, train_size), rep(1, nrow(model_data) - train_size)))
+table(split) 
+train_data <- model_data[split == 0, ]      
+test_data <- all_data[split == 1, ]
+
+##########################
+library(rpart)
+library(rpart.plot)
+
+prune_control <- rpart.control(maxdepth = 10) # minsplit = 10
+m <- rpart(pm10 ~ ., data = train_data, method = "class", control = prune_control)
+
+rpart.plot(m)
+plotcp(m)
+m_pruned <- prune(m, cp = 0.075)
+rpart.plot(m_pruned, type = 3, box.palette = c("green", "red"), fallen.leaves = TRUE)
+
+test_data$predict <- predict(m_pruned, test_data, type = "class")
+mean(test_data$predict == test_data$pm10, na.rm = TRUE)  # this is a case of predicting rare events, need to revisit.
+
+##########################
+library(randomForest)
+
+mrf <- randomForest(pm10 ~ ., data = train_data,
+                  ntree = 500)
+
+test_data$predict <- predict(mrf, test_data, type = "class")
+mean(test_data$predict == test_data$pm10, na.rm = TRUE)
+
+##########################
+
+all_data$predict <- predict(m_pruned, all_data, type = "class") 
+
+summary_stats_meteo <- all_data %>% 
+  select(year, predict) %>% 
+  group_by(year) %>%
+  summarise(
+    low_exceedance = sum(predict == "pm10_below_50", na.rm = TRUE),
+    high_exceedance = sum(predict == "pm10_above_50", na.rm = TRUE)
+  )
+
+summary_out <- summary_stats %>% left_join(summary_stats_meteo, by = "year")
+# bar chart showing proportions
--- annual-reporting/functions.R
+++ annual-reporting/functions.R
@@ -18,7 +18,7 @@ get_rainfall_data <- function(site,
                               from = from, 
                               to = to) {
   # Get rainfall data from Hilltop Server.
-  get_data_site_measurement(site = site, measurement = "Rainfall", method = "Total", time_interval = NA, from = from, to = to, interval = "1 day", alignment = "00:00") %>% 
+  get_data_site_measurement(site = site, measurement = "Rainfall", method = "Total", from = from, to = to, interval = "1 day") %>% 
     rename(rainfall = value) %>%
     arrange(datetime) %>%
     mutate(
@@ -34,20 +34,30 @@ determine_meteo_variable <- function(meteo,
                                      hr_end = 24, 
                                      offset_day = 0, 
                                      allowable_missing_hrs = 1, 
-                                     FUN = mean) {
+                                     FUN = mean, 
+                                     filter_value = NA) {
   # Determines additional meteorological variables based on defined times of day.
   # hr_start : left bound of the hour filter.
   # hr_end : right bound of the hour filter.
   # offset_day : if required to return the average for the preceding (negative)/following days (positive).
   # allowable_missing_hrs : how many hours of the period can be missing.
-  # func : function to use when calculating the variable. 
-
-  meteo %>% 
+  # FUN : function to use when calculating the variable. 
+  # filter_value : additional variable if counting (through a sum) the number of hours the value is less than this filter_value.
+  
+  meteo <- meteo %>% 
     filter(hour >= hr_start & hour < hr_end) %>% 
     group_by(date) %>% 
     mutate(n_hrs_day = n()) %>% 
     ungroup() %>% 
-    filter(n_hrs_day >= hr_end - hr_start - allowable_missing_hrs) %>% # apply allowable_missing_hrs
+    filter(n_hrs_day >= hr_end - hr_start - allowable_missing_hrs) # apply allowable_missing_hrs
+    
+  if (!is.na(filter_value)) {
+    meteo <- meteo %>% 
+      filter(value < filter_value) %>% 
+      mutate(value = 1) # to be used with sum FUN.
+  }
+  
+  meteo %>% 
     group_by(date) %>% 
     summarise(value = FUN(value, na.rm = TRUE)) %>% 
     transmute(date, value) %>% 
@@ -59,9 +69,11 @@ get_meteorological_data <- function(site,
                                     from = from, 
                                     to = to) {
   # Get wind and temperature data from Hilltop Server.
+  meteo_data <- tibble(date = seq(ymd(from), ymd(to), by = "1 day"))
   
-  ###### Wind
-  wind_mps <- get_data_site_measurement(site = site, measurement = "Wind Speed (hourly)", method = "Average", time_interval = NA, from = from, to = to, interval = "1 hour", alignment = "00:00") %>% 
+  ##### Wind Speed
+  wind_mps <- get_data_site_measurement(site = site, measurement = "Wind Speed (hourly)", 
+                                        method = "Average", from = from, to = to, interval = "1 hour") %>% 
     arrange(datetime) %>%
     mutate(
       value = round(value/3.6, digits = 2), # convert to mps.
@@ -69,73 +81,55 @@ get_meteorological_data <- function(site,
     ) %>%
     select(datetime, date, hour, value) 
 
-  # 24-hour average (midnight to midnight)
-  determine_meteo_variable(wind_mps, hr_start = 0, FUN = max)
-  determine_meteo_variable(wind_mps, hr_start = 17)
-  determine_meteo_variable(wind_mps, hr_start = 20)
-  determine_meteo_variable(wind_mps, hr_start = 6, hr_end = 12)
+  meteo_data <- meteo_data %>% 
+    left_join(determine_meteo_variable(wind_mps) %>% rename(wind_mps_24h_avg = value), by = "date") %>%  # 24-hour average (midnight to midnight)
+    left_join(determine_meteo_variable(wind_mps, hr_start = 17) %>% rename(wind_mps_7h_avg = value), by = "date") %>%  # 7-hour average (5 pm to midnight)
+    left_join(determine_meteo_variable(wind_mps, hr_start = 20) %>% rename(wind_mps_4h_avg = value), by = "date") %>%   # 4-hour average (8 pm to midnight)
+    left_join(determine_meteo_variable(wind_mps, hr_start = 6, hr_end = 12) %>% rename(wind_mps_6h_avg_am = value), by = "date") %>%  # 6-hour average (6 am to midday)
+    left_join(determine_meteo_variable(wind_mps, hr_start = 18, offset_day = -1) %>% rename(wind_mps_6h_avg_pm_preceeding = value), by = "date") %>%  # 6-hour average preceding day (6 pm to midnight)
+    left_join(determine_meteo_variable(wind_mps, FUN = min) %>% rename(wind_mps_min = value), by = "date") %>%  # Minimum 1-hour (Midnight to midnight)
+    left_join(determine_meteo_variable(wind_mps, FUN = max) %>% rename(wind_mps_max = value), by = "date") %>%  # Maximum 1-hour (Midnight to midnight)
+    left_join(determine_meteo_variable(wind_mps, hr_start = 16, hr_end = 17) %>% rename(wind_mps_1h_avg_16 = value), by = "date") %>%  # Hourly average (Hour ending 5 pm, start 1600)
+    left_join(determine_meteo_variable(wind_mps, hr_start = 19, hr_end = 20) %>% rename(wind_mps_1h_avg_19 = value), by = "date") %>%  # Hourly average (Hour ending 8 pm, start 1900)
+    left_join(determine_meteo_variable(wind_mps, hr_start = 17, FUN = sum, filter_value = 1) %>% rename(wind_mps_nhrs_less1 = value), by = "date") %>%  # No. hours (5 pm to midnight), < 1 ms-1
+    left_join(determine_meteo_variable(wind_mps, hr_start = 17, FUN = sum, filter_value = 2) %>% rename(wind_mps_nhrs_less2 = value), by = "date") %>%  # No. hours (5 pm to midnight), < 2 ms-1
+    left_join( determine_meteo_variable(wind_mps, hr_start = 17, FUN = sum, filter_value = 3) %>% rename(wind_mps_nhrs_less3 = value), by = "date") # No. hours (5 pm to midnight), < 3 ms-1
   
-  # 7-hour average (5 pm to midnight)
- 
-  # 4-hour average (8 pm to midnight)
-
-  # 6-hour average (6 am to midday)
-
-  # 6-hour average preceding day (6 pm to midnight)
-  
-  # Minimum 1-hour (Midnight to midnight)
-  
-  # Maximum 1-hour (Midnight to midnight)
-  
-  # Hourly average (Hour ending 5 pm, 1600)
-  
-  # Hourly average (Hour ending 8 pm, 1900)
-  
-  # No. hours (5 pm to midnight)
-  # < 1 ms-1
-  # < 2 ms-1
-  # < 3 ms-1
-  
-  ###### Temperature
-  temp <- get_data_site_measurement(site = site, measurement = "Air Temperature (continuous)", method = "Average", time_interval = NA, from = from, to = to, interval = "1 hour", alignment = "00:00") %>% 
-    rename(air_temp = value) %>%
+  #### Wind Direction
+  wind_dir <- get_data_site_measurement(site = site, measurement = "Wind Direction (hourly)", 
+                                        method = "Average", from = from, to = to, interval = "1 hour") %>% 
     arrange(datetime) %>%
     mutate(
-      wind_speed = round(air_temp, digits = 2)
+      hour = hour(datetime)
     ) %>%
-    mutate(date = as.Date(datetime)) %>% 
-    select(date, air_temp)
+    select(datetime, date, hour, value) 
   
+  meteo_data <- meteo_data %>% 
+    left_join(determine_meteo_variable(wind_dir, hr_start = 16, hr_end = 17) %>% rename(wind_dir_1h_avg_16 = value), by = "date") %>%  # Hourly average (Hour ending 5 pm, start 1600)
+    left_join(determine_meteo_variable(wind_dir, hr_start = 19, hr_end = 20) %>% rename(wind_dir_1h_avg_19 = value), by = "date") # Hourly average (Hour ending 8 pm, start 1900)
+
+  ###### Temperature
+  temp <- get_data_site_measurement(site = site, measurement = "Air Temperature (continuous)", 
+                                    method = "Average", from = from, to = to, interval = "1 hour") %>% 
+    arrange(datetime) %>%
+    mutate(
+      hour = hour(datetime)
+    ) %>%
+    select(datetime, date, hour, value) 
   
-  # 24-hour average (midnight to midnight)
+  meteo_data <- meteo_data %>% 
+    left_join(determine_meteo_variable(temp) %>% rename(temp_degc_24h_avg = value), by = "date") %>%  # 24-hour average (midnight to midnight)
+    left_join(determine_meteo_variable(temp, hr_start = 17) %>% rename(temp_degc_7h_avg = value), by = "date") %>% # 7-hour average (5 pm to midnight)
+    left_join(determine_meteo_variable(temp, hr_start = 20) %>% rename(temp_degc_4h_avg = value), by = "date") %>%   # 4-hour average (8 pm to midnight)
+    left_join(determine_meteo_variable(temp, FUN = min) %>% rename(temp_degc_min = value), by = "date") %>%  # Minimum 1-hour (Midnight to midnight)
+    left_join(determine_meteo_variable(temp, FUN = max) %>% rename(temp_degc_max = value), by = "date") %>%  # Maximum 1-hour (Midnight to midnight)
+    left_join(determine_meteo_variable(temp, offset_day = 1, FUN = min) %>% rename(temp_degc_1h_min_following = value), by = "date") %>% # Minimum following day 1-hour (Midnight to midnight)
+    left_join(determine_meteo_variable(temp, hr_start = 16, hr_end = 17) %>% rename(temp_degc_1h_avg_16 = value), by = "date") %>%  # Hourly average (Hour ending 5 pm, start 1600)
+    left_join(determine_meteo_variable(temp, hr_start = 19, hr_end = 20) %>% rename(temp_degc_1h_avg_19 = value), by = "date") %>%  # Hourly average (Hour ending 8 pm, start 1900)
+    left_join(determine_meteo_variable(temp, hr_start = 17, FUN = sum, filter_value = 1) %>% rename(temp_degc_nhrs_less1 = value), by = "date") %>%  # No. hours (5 pm to midnight), < 1 deg C
+    left_join(determine_meteo_variable(temp, hr_start = 17, FUN = sum, filter_value = 5) %>% rename(temp_degc_nhrs_less5 = value), by = "date") %>%  # No. hours (5 pm to midnight), < 5 deg C
+    left_join(determine_meteo_variable(temp, hr_start = 17, FUN = sum, filter_value = 10) %>% rename(temp_degc_nhrs_less10 = value), by = "date") %>%  # No. hours (5 pm to midnight), < 10 deg C
+    mutate(temp_degc_max_less_min_following = temp_degc_max - temp_degc_1h_min_following) # Max sample day less min day following 1-hour (Midnight to midnight)
   
-  # 7-hour average (5 pm to midnight)
-  
-  # 4-hour average (8 pm to midnight)
-  
-  # Minimum 1-hour (Midnight to midnight)
-  
-  # Minimum following day 1-hour (Midnight to midnight)
-  
-  # Max sample day less min day following 1-hour (Midnight to midnight)
-  
-  # Maximum 1-hour (Midnight to midnight)
-  
-  # Hourly average (Hour ending 5 pm)
-  
-  # Hourly average (Hour ending 8 pm)
-  
-  # No. hours (5 pm to midnight)
-  # < 1 deg C
-  # < 5 deg C
-  # < 10 deg C
-  
-  
-  
-  # Wind direction
-  # Hourly average (Hour ending 5 pm)
-  
-  # Hourly average (Hour ending 8 pm)
-  
-  wind %>% left_join(temp, by = "date")
+  meteo_data
 }
