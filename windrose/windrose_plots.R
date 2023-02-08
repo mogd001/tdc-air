9ac5ddd57bcafc6aec0de56db4c209a8db7cb0a6
matt.ogden <mogd001@gmail.com>
2023-02-08 13:31:59 +1300

    Windrose update

--- windrose/windrose_plots.R
+++ windrose/windrose_plots.R
@@ -4,105 +4,202 @@ library(lubridate)
 library(glue)
 library(rvest)
 
+library(tdcR)
+
 # https://bookdown.org/david_carslaw/openair/wind-roses.html
 ??windRose
 
-# Load example data
-wind_data <- read.csv("data/example_wind_data.csv") %>%
-  as_tibble() %>%
-  mutate(nzst = dmy_hm(nzst, tz = "NZ"), 
-         date = as.Date(nzst, tz = "NZ"),
-         date_hour = floor_date(nzst, "1 hour"))
+from <- "Data start" #from <- "Data start"
+to <- "Data end" #to <- "Data end"
+computed <- now()
+breaks <- c(-999, 5, 8, 11, 14, 17, 20, 999) # breaks between wind speeds
 
-# Single windrose plot
-png(file=glue("single-windrose-plot.png"))  
-windRose(wind_data, ws = "spd", wd = "dir",
-         breaks = c(1, 5, 10, 15, 20, 25, 999),
-         angle = 45,
-         paddle = FALSE,
-         width = 2.0,
-         auto.text = FALSE,
-         annotate = TRUE,
-         grid.line = 5,
-         key = list(labels = c("1-5","5-10","10-15","15-20","20-25",">25"), header="Windrose Plot ", footer="Wind Speed (km/h)",
-                    plot.style = c("ticks", "border"), fit = "all", height = 1, space = "top"),
-         par.settings = list(axis.line=list(col="lightgray")),
-         col = c("#012CFF", "#00D5F7", "#7CFD7F", "#FDE801", "#FF4503", "#7E0100"))
-dev.off()
+temp <- embed(breaks, 2)[, 2:1]
+labels <- c(paste0("ws ", temp[, 1], " - ", temp[, 2])) %>%
+  as.character() 
 
-# All windrose plots for specific date
-target_date = as.Date("2021-06-03")
-animation_data <- wind_data %>% 
-  filter(date ==  target_date)
+# update first and last labels manually
+labels[1] <- paste0("ws <=", breaks[2]) 
+labels[length(labels)] <- paste0("ws >", breaks[length(breaks)-1])
 
-for (d in unique(animation_data$date_hour)){
-  d <- as.POSIXct(d, origin = "1970-01-01 00:00:00", tz = "NZ")
-  hr_wind_data = animation_data %>%
-    filter(date_hour == d)
-  
-  fname <- gsub(":","", d)
-  png(file=glue("plot-outputs/{fname}.png"))
-  windRose(hr_wind_data, ws = "spd", wd = "dir",
-           breaks = c(1,5,10,15, 20, 25, 999),
-           angle = 45,
-           paddle = FALSE,
-           width = 2.0,
-           auto.text = FALSE,
-           annotate = TRUE,
-           grid.line = 5,
-           key = list(labels = c("1-5","5-10","10-15","15-20","20-25",">25"), header=paste("Windrose Plot ", d), footer="Wind Speed (km/h)",
-                      plot.style = c("ticks", "border"),
-                      fit = "all", height = 1, space = "top"),
-           par.settings = list(axis.line=list(col="lightgray")),
-           col = c("#012CFF", "#00D5F7", "#7CFD7F", "#FDE801", "#FF4503", "#7E0100"))
-  dev.off()
+col_names <- c()
+for (i in 1:(length(breaks) - 1)) {
+  x <- paste0("Interval", i)
+  col_names <- append(col_names, x)
 }
 
-# Test alternative windrose plot (polar plot) based on DataCamp exercise
-wind <- wind_data
+summary <- tibble()
 
-# Plot distribution of speed
-summary(wind$spd)
-boxplot(wind$spd)
+sites <- c("HY Tasman Bay Buoy", 
+           "HY Golden Bay Buoy", 
+           "HY Takaka at Aerodrome",
+           "HY Richmond Weather at Race Course")
 
-outliers <- boxplot(wind$spd, plot=FALSE)$out
-wind<-wind
-wind<- wind[-which(wind$spd %in% outliers),]
-boxplot(wind$spd)
+for (site in sites) {
+  print(site)
+  if (site == "HY Golden Bay Buoy") {
+    ws_var <- "Wind Speed (6m) [Wind Speed (6m)]"
+    wd_var <- "Wind Direction (6m) [Wind Direction (6m)]"
+  } else if (site %in% c("HY Richmond Weather at Race Course", "HY Motueka at Sportspark")) {
+    ws_var <- "Wind Speed (hourly)"
+    wd_var <- "Wind Direction (hourly)"
+  } else {
+    ws_var <- "Wind Speed"
+    wd_var <- "Wind Direction"
+  }
 
-library(plotly)
-ggplotly(ggplot(wind, aes(x = spd)) + 
-  geom_density() +
-  coord_cartesian(xlim = c(0, 9), ylim = c(0, 0.3), expand = 0) +
-  theme_classic())
+  wind_speed <- get_data_site_measurement(
+    site = site,
+    measurement = ws_var,
+    from = from,
+    to = to
+  )
 
-# https://community.rstudio.com/t/convert-wind-direction-degrees-into-factors-in-a-data-frame/14636/2
-url <- 'http://snowfence.umn.edu/Components/winddirectionanddegreeswithouttable3.htm'
-page <- read_html(url)
-directions_raw <- page %>% html_node('td table') %>% html_table(header = TRUE)
+  wind_direction <- get_data_site_measurement(
+    site = site,
+    measurement = wd_var,
+    from = from,
+    to = to
+  )
 
-directions <- directions_raw %>% 
-  set_names(~tolower(sub(' Direction', '', .x))) %>% 
-  slice(-1) %>% 
-  separate(degree, c('degree_min', 'degree_max'), sep = '\\s+-\\s+', convert = TRUE)
+  wind_data <- wind_speed %>%
+    transmute(datetime, wind_speed = value) %>%
+    left_join(select(wind_direction, c(datetime, value)), by = "datetime") %>%
+    rename(wind_direction = value) %>%
+    mutate(
+      wind_speed = wind_speed / 3.6, # convert km/h to m/s
+      date = as.Date(datetime, tz = "Etc/GMT-12")
+    )
 
-directions
+  wrp <- windRose(wind_data,
+    ws = "wind_speed", wd = "wind_direction",
+    #type = "month",  # break down plot by time period, see cutData for potential ways of cutting the data
+    breaks = breaks,
+    angle = 22.5,
+    paddle = FALSE,
+    offset = 15,
+    width = 2.0,
+    auto.text = FALSE,
+    annotate = TRUE,
+    grid.line = 5,
+    key = list(
+      labels = labels, header = paste0(site, " ", from, " to ", to, " computed at: ", computed), footer = "Wind Speed (m/s)",
+      plot.style = c("ticks", "border"), fit = "all", height = 1, space = "top"
+    ),
+    par.settings = list(axis.line = list(col = "lightgray"))
+  )
 
-wind <- wind %>% 
+  png(
+    file = glue("outputs/{site}-single-windrose-plot.png"),
+    width = 750,
+    height = 750,
+    res = 72
+  )
+  print(wrp$plot)
+  dev.off()
+
+  out_df <- wrp$data %>%
+    ungroup() %>%
+    select(c("wd", col_names)) %>%
+    setNames(c("wd", labels)) %>%
+    mutate()
+
+  wd <- out_df$wd
+  out_df <- out_df %>%
+    select(-wd)
+
+  out_df <- cbind(out_df[1], out_df[3:length(breaks) - 1] - out_df[2:length(breaks) - 2]) %>%
+    mutate(wd = wd) %>%
+    relocate(wd) %>% 
+    filter(!(wd < 0)) # remove -999
+  
+  write_csv(out_df, glue("outputs/{site}-windrose-plot-data.csv"))
+  
+  summary <- summary %>% 
+    bind_rows(tibble(site = site, start_time = min(wind_data$datetime), end_time = max(wind_data$datetime)))
+  
+}
+
+# Restricted sites 
+library(zoo)
+library(broom)
+library(Hilltop)
+
+restricted_sites <- c("HY Farewell Spit (AWS)", "HY Seperation Point (AWS)")
+
+for (site in restricted_sites) {
+  
+  data <- HilltopData("M:/Datafiles/Archive/Restricted Access Data.hts") 
+  wind_speed <- GetData(data, site, "Wind Speed", "", "") %>% 
+    tidy() %>% 
+    mutate(datetime = force_tz(index, tz = "Etc/GMT-12")) %>% 
+    select(datetime, value)
+  wind_direction <- GetData(data, site, "Wind Direction", "", "") %>% 
+    tidy() %>% 
+    mutate(datetime = force_tz(index, tz = "Etc/GMT-12")) %>% 
+    select(datetime, value)
+
+  disconnect(data)
+  
+  wind_data <- wind_speed %>%
+    transmute(datetime, wind_speed = value) %>%
+    left_join(select(wind_direction, c(datetime, value)), by = "datetime") %>%
+    rename(wind_direction = value) %>%
+    mutate(
+      wind_speed = wind_speed / 3.6, # convert km/h to m/s
+      date = as.Date(datetime, tz = "Etc/GMT-12")
+    )
+  
+  wrp <- windRose(wind_data,
+                  ws = "wind_speed", wd = "wind_direction",
+                  #type = "month",  # break down plot by time period, see cutData for potential ways of cutting the data
+                  breaks = breaks,
+                  angle = 22.5,
+                  paddle = FALSE,
+                  offset = 15,
+                  width = 2.0,
+                  auto.text = FALSE,
+                  annotate = TRUE,
+                  grid.line = 5,
+                  key = list(
+                    labels = labels, header = paste0(site, " ", from, " to ", to, " computed at: ", computed), footer = "Wind Speed (m/s)",
+                    plot.style = c("ticks", "border"), fit = "all", height = 1, space = "top"
+                  ),
+                  par.settings = list(axis.line = list(col = "lightgray"))
+  )
+  
+  png(
+    file = glue("outputs/{site}-single-windrose-plot.png"),
+    width = 750,
+    height = 750,
+    res = 72
+  )
+  print(wrp$plot)
+  dev.off()
+  
+  out_df <- wrp$data %>%
+    ungroup() %>%
+    select(c("wd", col_names)) %>%
+    setNames(c("wd", labels)) %>%
+    mutate()
+  
+  wd <- out_df$wd
+  out_df <- out_df %>%
+    select(-wd)
+  
+  out_df <- cbind(out_df[1], out_df[3:length(breaks) - 1] - out_df[2:length(breaks) - 2]) %>%
+    mutate(wd = wd) %>%
+    relocate(wd)
+  write_csv(out_df, glue("outputs/{site}-windrose-plot-data.csv"))
+  
+  summary <- summary %>% 
+    bind_rows(tibble(site = site, start_time = min(wind_data$datetime), end_time = max(wind_data$datetime)))
+  
+}
+
+summary %>% 
   mutate(
-    spd_bin = cut(spd, breaks=c(0, 2, 6, 8, 10)),
-    dir_bin = cut(
-      wd, 
-      breaks = c(0, directions$degree_max, 360), 
-      labels = c(directions$cardinal, 'N')),
-    ws = spd,
-    wd = dir
-    ) %>%
-  drop_na(spd_bin, dir_bin)
-
-polarFreq(wind)
-windRose(wind, paddle = FALSE, key = list(footer="Wind Speed (km/h)"))
-
-ggplot(wind, aes(dir_bin, fill = spd_bin)) +
-  geom_bar(width = 1) +
-  coord_polar(start = -pi/16)
+    start_time = force_tz(start_time, tz = "UTC"),
+    end_time = force_tz(end_time, tz = "UTC")
+  ) %>% 
+write_csv(glue("outputs/windrose_summary.csv"))
