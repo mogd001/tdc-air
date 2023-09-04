05109d2d3057103b88e4bc23aa43192e88afcc77
matt.ogden <mogd001@gmail.com>
2023-09-04 16:51:30 +1200

    Added documentation

--- process-bam-data/main.R
+++ process-bam-data/main.R
@@ -1,18 +1,17 @@
-##### Process BAM (Beta Attenuation Mass/Monitor) Data #####
+##### Process BAM 5028i (Beta Attenuation Mass/Monitor) Data from Plunket site
 #
 # Matt Ogden, September 2022
 #
 ##################
 
-# TO COMPLETE DOCUMENTATION
-#
-#
-#
+# This script merges datafiles downloaded from the BAM 5028i Richmond Central at Plunket, preparing it for upload to hilltop.
+# Download the data and save to the directory as shown below.
 
 # Processing directory (where .dat files are located)
 #directory <- "M:/Datafiles/Downloads&OtherSources/AIRQUALITY/Richmond/Plunket/5028i BAM/(024) 5028i Download 02-08-2022"
 #directory <- "M:/Datafiles/Downloads&OtherSources/AIRQUALITY/Richmond/Plunket/5028i BAM/(025) 5028i Download 15-09-2022"
-directory <- "M:/Datafiles/Downloads&OtherSources/AIRQUALITY/Richmond/Plunket/5028i BAM/(027) 5028i Download 16-05-2023"
+#directory <- "M:/Datafiles/Downloads&OtherSources/AIRQUALITY/Richmond/Plunket/5028i BAM/(027) 5028i Download 16-05-2023"
+directory <- "M:/Datafiles/Downloads&OtherSources/AIRQUALITY/Richmond/Plunket/5028i BAM/(028) 5028i Download 04-09-2023"
 
 write_comments_to_database <- FALSE
 
